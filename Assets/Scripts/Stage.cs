da859270b24d211a3c3148d56ce5e41539100475
David <davidf1212@gmail.com>
2015-10-15 16:53:22 -0400

    cell size

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class Stage : MonoBehaviour {
 	public GameObject dot;
-	public Vector2 gridSize;
+	public float gridSize;
+	public float cellSize;
 
 	void Start () {
+		float rectSize = gameObject.GetComponent<RectTransform> ().sizeDelta.x;
+		cellSize = rectSize / gridSize;
+
 		GameObject d;
 		Vector3 pos;
-		for (int i = 0; i <= gridSize.x; i++) {
-			for (int j = 0; j <= gridSize.y; j++) {
-				Vector2 r = gameObject.GetComponent<RectTransform>().sizeDelta;
-				pos = new Vector3(r.x * (i/gridSize.x - 0.5f), r.y * (j/gridSize.y - 0.5f), 0);
+		for (float i = 0; i <= rectSize; i += cellSize) {
+			for (float j = 0; j <= rectSize; j += cellSize) {
+				pos = new Vector3(i-rectSize/2, j-rectSize/2, 0);
 				d = Instantiate (dot) as GameObject;
 				d.transform.parent = gameObject.transform;
 				d.transform.localPosition = pos;
