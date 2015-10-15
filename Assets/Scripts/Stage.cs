94c559cb93868059101b56cad2060ac77fab44c8
Ayub <ayub@uken.com>
2015-10-15 17:40:18 -0400

    block prefab

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -1,28 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Stage : MonoBehaviour {
 	public GameObject dot;
-	public float gridSize;
+	public GameObject block;
+	public int gridSize;
 	public float cellSize;
+	private float rectSize;
+
+	private GameObject[,] grid;
 
 	void Start () {
-		float rectSize = gameObject.GetComponent<RectTransform> ().sizeDelta.x;
+		grid = new GameObject[gridSize + 1, gridSize + 1];
+
+
+		rectSize = gameObject.GetComponent<RectTransform> ().sizeDelta.x;
 		cellSize = rectSize / gridSize;
 
 		GameObject d;
 		Vector3 pos;
-		for (float i = 0; i <= rectSize; i += cellSize) {
-			for (float j = 0; j <= rectSize; j += cellSize) {
-				pos = new Vector3(i-rectSize/2, j-rectSize/2, 0);
+		for (int i = 0; i <= gridSize; i++) {
+			for (int j = 0; j <= gridSize; j++) {
+				pos = new Vector3(i*cellSize-rectSize/2, j*cellSize-rectSize/2, 0);
 				d = Instantiate (dot) as GameObject;
 				d.transform.parent = gameObject.transform;
 				d.transform.localPosition = pos;
+
+				grid[i,j] = d;
 			}
 		}
+		Debug.Log(rectSize);
+		// Instantiate first block and set position.
+		GameObject firstBlock = Instantiate (block) as GameObject;
+		firstBlock.transform.parent = gameObject.transform;
+		firstBlock.transform.localPosition = coordToPos(0,0);
+
+
+
 	}
 
 	void Update () {
 	
+	}
+
+	public Vector2 coordToPos(int x, int y){
+		Vector2 pos = grid[x,y].transform.localPosition;
+		Debug.Log(pos);
+		pos.x += cellSize / 2;
+		pos.y += cellSize / 2;
+		Debug.Log(pos);
+		return pos;
+
 	}
 }
---
+++ Assets/blockManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockManager : MonoBehaviour {
+	Stage stageScript ;
+	float cellSize ;
+	// Use this for initialization
+	void Start () {
+
+		stageScript = gameObject.transform.parent.gameObject.GetComponent<Stage>();
+		cellSize = stageScript.cellSize;
+
+		// instantiate it 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
