6bb4a92ec7c67f39cb564ec9ff32b37b366f2e69
David <davidf1212@gmail.com>
2015-10-16 10:38:31 -0400

    spawn blocks method

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+	public enum Direction { Up, Down, Left, Right }
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -8,13 +8,13 @@ public class Stage : MonoBehaviour {
 	public GameObject block;
 	public int gridSize;
 	public float cellSize;
+	public float blockSpeed = 0.5f;
 
 	private float rectSize;
 	private GameObject[,] grid;
 
 	void Start () {
 		grid = new GameObject[gridSize + 1, gridSize + 1];
-
 
 		rectSize = gameObject.GetComponent<RectTransform> ().sizeDelta.x;
 		cellSize = rectSize / gridSize;
@@ -27,32 +27,41 @@ public class Stage : MonoBehaviour {
 				d = Instantiate (dot) as GameObject;
 				d.transform.parent = dots.transform;
 				d.transform.localPosition = pos;
-
 				grid[i,j] = d;
 			}
 		}
-		Debug.Log(rectSize);
-
-		// Instantiate first block and set position.
-		GameObject firstBlock = Instantiate (block) as GameObject;
-		firstBlock.transform.parent = gameObject.transform;
-		firstBlock.transform.localPosition = coordToPos(3,3);
-		firstBlock.transform.localScale = new Vector2(cellSize, cellSize);
-
-
+		SpawnBlock ();
+		SpawnBlock ();
+		SpawnBlock ();
 	}
 
 	void Update () {
 	
 	}
 
-	public Vector2 coordToPos(int x, int y){
-		Vector2 pos = grid[x,y].transform.localPosition;
-		Debug.Log(pos);
+	public void SpawnBlock() {
+		int x, y;
+		if (Random.value > 0.5f) { // vertical trajectory
+			x = Random.Range(-1, gridSize);
+			y = Random.value > 0.5f ? -1 : gridSize;
+		} else { // horizontal trajectory
+			x = Random.value > 0.5f ? -1 : gridSize;
+			y = Random.Range(-1, gridSize);
+		}
+		AddBlock (x, y);
+	}
+
+	public void AddBlock(int x, int y){
+		GameObject newBlock = Instantiate (block) as GameObject;
+		newBlock.transform.parent = transform;
+		newBlock.transform.localPosition = CoordToPos (x, y);
+		newBlock.transform.localScale = new Vector2 (cellSize, cellSize);
+	}
+
+	public Vector2 CoordToPos(int x, int y){
+		Vector2 pos = new Vector2 (x * cellSize - rectSize / 2, y * cellSize - rectSize / 2);
 		pos.x += cellSize / 2;
 		pos.y += cellSize / 2;
-		Debug.Log(pos);
 		return pos;
-
 	}
 }
